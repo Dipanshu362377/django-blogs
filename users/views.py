53bf5b8e09aaf9bc5b33107e736a28e9aed62a86
Dipanshu Chauhan <chauhandipanshu005@gmail.com>
2025-10-12 18:59:23 +0530

    user registration page

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .form import UserRegisterForm
 
 def register(request):
@@ -9,8 +10,12 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created successfully for {username}!')
-            return redirect('home')  # Redirect to login after successful registration
+            return redirect('login')  
     else:
         form = UserRegisterForm()
     
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')

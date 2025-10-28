3a2084f176f61b2cb49c7ec2381b46580780254d
Dipanshu Chauhan <chauhandipanshu005@gmail.com>
2025-10-28 23:24:25 +0530

    list and detail view

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -11,6 +12,15 @@ def home(request):
         'cont': Post.objects.all()
     }
     return render(request, 'posts/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/home.html'
+    context_object_name = 'cont'
+    ordering = ['-date_posted']
+    
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request, 'posts/about.html', {'title': 'About'})
